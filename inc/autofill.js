f996fefa0169db94a1018a9e8d305b3286d5a5eb
Sango <74567441+moistp1ckle@users.noreply.github.com>
2022-06-11 07:01:32 -0500

    Update autofill.js

--- inc/autofill.js
+++ inc/autofill.js
@@ -51,7 +51,10 @@ if (localStorage.getItem("autofill_mentions") != 0) {
        if ($text.text() != null && $text.text().includes("@everyone")) {
            // Shoutbox.mentionSound.play(); disabled cuz annoying
            $msg.addClass("mention");
-           $text.html($text.html().replace(/(<a href\=\"https:\/\/i.imgur.com\/.*?)>  <a (href=\"https:\/\/i.imgur.com\/.*?)<\/a>/g, '@<a rel=”noreferrer” \1<\/a>').replace(/@everyone/g, '@<a href="/Sango" onclick="return false;"><span class="registered_rank">everyone</span></a>'));
+           $text.html($text.html().replace(/@everyone/g, '@<a href="/Sango" onclick="return false;"><span class="registered_rank">everyone</span></a>'));
+       }
+        if ($text.text() != null && $text.text().includes("i.imgur.com")) {
+           $text.html($text.html().replace(/<a (href=\"https:\/\/i.imgur.com\/.*?)<\/a>/g, '<a rel="noreferrer" $1<\/a>'));
        }
 
        if ((uid=$msg.attr("data-user")) != null && uid != "" && Object.values(uids).indexOf(uid) == -1) {
